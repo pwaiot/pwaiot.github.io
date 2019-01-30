839167620c6199a847522217cf18e58c69a475da
ValterFC <valter@accellog.com>
2019-01-30 11:07:35 -0200

    Melhorias

--- pwa/src/precache-manifest.js
+++ pwa/src/precache-manifest.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = [
-  {
-    "revision": "v.2019.01.28-1",
+  /*{
+    "revision": "v.2019.01.30-1",
     "url": "js/script.js"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
+    "url": "index.html"
+  },*/
+  {
+    "revision": "v.2019.01.30-1",
     "url": "css/demo.css"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "css/style.css"
   },
   {
-    "revision": "v.2019.01.28-1",
-    "url": "index.html"
-  },
-  {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon310.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon192.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon180.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon144.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon120.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/icon96.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/favicon96.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/favicon32.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/favicon16.png"
   },
   {
-    "revision": "v.2019.01.28-1",
+    "revision": "v.2019.01.30-1",
     "url": "statics/icons/favicon.ico"
   }
 ];
